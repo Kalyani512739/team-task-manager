9728c7802e4d849f30768a1e42ab7f13eace39a1
Kalyani512739 <kaly32184@gmail.com>
2026-04-30 20:09:22 +0530

    added frontend

--- team-task-manager/server.js
+++ team-task-manager/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
